c1693369e1159fdbaa38655fca4c254023aebe80
Andrew ONeill <andy@evolve757.com>
2025-09-12 21:52:30 -0400

    completed exercise 2

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ArraysAndLists
 {
@@ -7,38 +8,31 @@ namespace ArraysAndLists
         static void Main(string[] args)
         {
             //TODO:
-
-            // Create an int array and populate it with numbers 1-10
             
+            var numbers = new int[10] {1,2,3,4,5,6,7,8,9,10};
+            var evens = new List<int>();
+            var odds = new List<int>();
+            foreach (var number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    evens.Add(number);
+                }
+                else
+                {
+                    odds.Add(number);
+                }
+            }
 
-            /* Create a list of type int
-             * Name the list "evens"
-             */
+            foreach (var number in evens)
+            {
+                Console.WriteLine(number);
+            }
 
-            
-            /* Create another list of type int
-             * Name the list "odds"
-             */
-            
-
-             /* Using either a foreach or for loop,
-             * iterate through the array you populated with 10 numbers.
-             * Inside the scope of the loop,
-             * check to see if each number in the array is even or odd.
-             * If the number is even, add it to the evens list.
-             * If the number is odd, add it to the odds list.
-             */
-
-
-
-            /* Using a foreach loop,
-             * display the numbers in your "evens" list
-             */
-            
-
-            /* Using a for loop,
-             * display the numbers in your "odds" list
-             */
+            for (int i = 0; i < odds.Count; i++)
+            {
+                Console.WriteLine(odds[i]);
+            }
         }
     }
 }
